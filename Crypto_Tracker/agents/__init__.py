10e082ed0a4518956f9b8eed435a5b5afb402b40
giuseppedavidde <davidde.giuseppe@gmail.com>
2026-02-06 23:10:41 +0100

    Stop tracking linked agents folders

--- Crypto_Tracker/agents/__init__.py
+++
@@ -1,5 +0,0 @@
-# agents/__init__.py
-from .cloud_manager import CloudManager
-from .cloud_ui import render_cloud_ui
-
-__all__ = ["CloudManager", "render_cloud_ui"]
--- Portfolio_Reader/agents/__init__.py
+++
@@ -1,6 +0,0 @@
-# agents/__init__.py
-from .ai_provider import AIProvider
-from .cloud_manager import CloudManager
-from .cloud_ui import render_cloud_sync_ui
-
-__all__ = ["AIProvider", "CloudManager", "render_cloud_sync_ui"]
